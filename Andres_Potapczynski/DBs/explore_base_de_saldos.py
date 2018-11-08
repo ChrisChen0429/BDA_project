72f9cc824e8b566cbf6e294c28079abdb62e3758
AndPotap <ap3635@columbia.edu>
2018-11-08 15:34:36 -0500

    Added files to generate core.txt

---
+++ Andres_Potapczynski/DBs/explore_base_de_saldos.py
@@ -0,0 +1,84 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Notes
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+(*) This script outputs the y-labels needed for the analysis.
+    It:
+        * reads the data bases
+        * subsets for the relevant columns only (the ones that contain
+            the number of delayed days and months
+        * merges the two data bases into one
+        * displays a summary table with the % of delayed contracts
+        * renames the columns
+        * outputs into a txt file
+"""
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Imports
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import os
+import numpy as np
+import pandas as pd
+from Andres_Potapczynski.Utils.eda import one_col_summary
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Open DBs
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+path = '/Users/andpotap/Documents/Columbia/BS/Risk-Managment'
+os.chdir(path)
+file_it = './DBs/BASE DE SALDOS FHIPO IT PESOS AGOSTO-18.xlsx.txt'
+file_s = './DBs/BASE DE SALDOS FHIPO SOCIAL PESOS_AGOSTO-18.xlsx.txt'
+
+data_it = pd.read_table(file_it)
+data_s = pd.read_table(file_s)
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Total number of mortages per data base
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+count_it = np.count_nonzero(data_it['numcred'].unique())
+print('\nIn Saldos IT we have: {} mortgages'.format(count_it))
+
+count_s = np.count_nonzero(data_s['numcred'].unique())
+print('\nIn Saldos Social we have: {} mortgages'.format(count_s))
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Subset the data into the columns that we care
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+selected_columns = ['numcred', 'omisos', 'dias_mora', 'num_pag_ef']
+data_it = data_it[selected_columns]
+data_it['origin'] = 'IT'
+data_s = data_s[selected_columns]
+data_s['origin'] = 'SOCIAL'
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Merge the data base into a single one
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data = pd.concat([data_s, data_it], ignore_index=True)
+
+count_all = np.count_nonzero(data['numcred'].unique())
+print('\nIn total we have: {} UNIQUE mortgages'.format(count_all))
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Compute the percentage of moratory
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+df_omisos = one_col_summary(data=data, column='omisos', col='origin')
+print('\nBelow is a summary that displays the percentage of delayed mort')
+print(df_omisos.head())
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Rename the columns
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+renaming_dict = {'numcred': 'mortgage_id',
+                 'omisos': 'months_wo',
+                 'dias_mora': 'days_wo',
+                 'num_pag_ef': 'total_w'}
+data = data.rename(columns=renaming_dict)
+data['y'] = 0
+data.loc[data['months_wo'] >= 1, 'y'] = 1
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Output the file
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+file_path = './DBs/core_y.txt'
+data.to_csv(file_path, sep='|', index=False)
+# ===========================================================================
---
+++ Andres_Potapczynski/DBs/generate_core_db.py
@@ -0,0 +1,151 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Notes
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+(*) This script explores the contents of the DBs given by the client
+(*) Beware! This file writes an output
+"""
+# TODO: missing to transform the entries into the most suitable format
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Imports
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import os
+import numpy as np
+import pandas as pd
+# from Utils.eda import one_col_summary
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Open DBs
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+path = '/Users/andpotap/Documents/Columbia/BS/Risk-Managment'
+os.chdir(path)
+data = pd.read_excel('./DBs/Base de originacion FHIPO.xlsx')
+# data = pd.read_excel('../DBs/Base de originacion FHIPO.xlsx', nrows=10)
+# data = pd.read_excel('../DBs/Base de originacion FHIPO.xlsx')
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Output list of columns to excel
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# pd.DataFrame(data.columns).to_excel('./DBs/column_list.xlsx',
+#                                     header=False,
+#                                     index=False)
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Explore columns contents
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# col = 'Nombre'
+# column = 'idpoliticasValidacion'
+
+# df = one_col_summary(data=data, column=column, col=col)
+
+# for colu in list(data.columns):
+#     df = one_col_summary(data=data, column=colu, col=col)
+#     print(df)
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Select columns to include and rename them
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+selected_columns = ['Nombre',
+                    'colonia_domicilio',
+                    'ciudad_domicilio',
+                    'entidad_federativa',
+                    'codigo_postal',
+                    'Genero',
+                    'numero_credito',
+                    'fecha_firma_credito',
+                    'factor_pago_roa',
+                    'factor_pago_rea',
+                    'tasa_interes',
+                    'antiguedad_empleo_titular',
+                    'ingresos_cliente_registrado_infonavit',
+                    'relacion_pago',
+                    'calificacion_infonavit',
+                    'nombre_vendedor',
+                    'valor_compra_venta',
+                    'colonia_compra',
+                    'ciudad_compra',
+                    'codigo_postal_compra',
+                    'entidad_federativa_compra',
+                    'valor_avaluo',
+                    'nueva_usada',
+                    'indice_riesgo',
+                    'tipo_credito',
+                    'antiguedad_laboral',
+                    'nombre_empresa',
+                    'edad',
+                    'plazo_propuesto',
+                    'calificacion_buro',
+                    'estado_civil_acreditado',
+                    'Calculado_edad',
+                    'Monto_credito_original_total_pesos']
+data_sub = data[selected_columns]
+renaming_dict = {'Nombre': 'mortgage_product',
+                 'colonia_domicilio': 'county',
+                 'ciudad_domicilio': 'city',
+                 'entidad_federativa': 'state',
+                 'codigo_postal': 'postal_code',
+                 'Genero': 'sex',
+                 'numero_credito': 'mortgage_id',
+                 'fecha_firma_credito': 'date_signed',
+                 'factor_pago_roa': 'factor_employed',
+                 'factor_pago_rea': 'factor_unemployed',
+                 'tasa_interes': 'interest_rate',
+                 'antiguedad_empleo_titular': 'months_employed',
+                 'ingresos_cliente_registrado_infonavit': 'client_income',
+                 'relacion_pago': 'ratio',
+                 'calificacion_infonavit': 'lender_score',
+                 'nombre_vendedor': 'vendor_name',
+                 'valor_compra_venta': 'asset_value',
+                 'colonia_compra': 'county_asset',
+                 'ciudad_compra': 'city_asset',
+                 'codigo_postal_compra': 'postal_code_asset',
+                 'entidad_federativa_compra': 'state_asset',
+                 'valor_avaluo': 'asset_market_value',
+                 'nueva_usada': 'new_used',
+                 'indice_riesgo': 'risk_index',
+                 'tipo_credito': 'credit_type',
+                 'antiguedad_laboral': 'months_employed',
+                 'nombre_empresa': 'employer_name',
+                 'edad': 'age',
+                 'plazo_propuesto': 'contract_length',
+                 'calificacion_buro': 'credit_score',
+                 'estado_civil_acreditado': 'married',
+                 'Calculado_edad': 'asset_age',
+                 'Monto_credito_original_total_pesos': 'contract_value'}
+data_sub = data_sub.rename(columns=renaming_dict)
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Load the y label data
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+file_y = './DBs/core_y.txt'
+data_y = pd.read_csv(file_y, sep='|')
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Combine the data sets
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+data_sub = pd.merge(left=data_sub, right=data_y, how='inner',
+                    on='mortgage_id')
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Output the cleaned data set
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+file_path = './DBs/core.txt'
+data_sub.to_csv(file_path, sep='|')
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Sample a small amount of the data set
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+n = data_sub.shape[0]
+per = 0.1
+m = round(n * per)
+np.random.seed(seed=12372763)
+selected_rows = np.random.choice(a=n, size=m, replace=False)
+data_sample = data_sub.iloc[selected_rows, :]
+# ===========================================================================
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# Output the random sample extract
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+file_path_sample = './DBs/core_sample.txt'
+data_sample.to_csv(file_path_sample, sep='|')
+# ===========================================================================
